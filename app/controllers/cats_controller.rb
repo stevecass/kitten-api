f853b58949cf64715954ff62f48b3905fd336e56
Steven Cassidy <steven.cassidy@gmail.com>
2016-01-04 22:27:08 -0500

    Implement a cats controller

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,43 @@
+class CatsController < ApplicationController
+  def index
+    cats = Cat.all
+    render json: cats
+  end
+
+  def show
+    cat = Cat.find(params[:id])
+    render json: cat
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+    update_and_show_cat cat
+  end
+
+  def create
+    cat = Cat.new
+    update_and_show_cat cat, 201
+  end
+
+  def destroy
+    cat = Cat.find(params[:id])
+    cat.destroy
+    render json: cat
+  end
+
+  private
+
+  def update_and_show_cat(cat, status=200)
+    cat.assign_attributes(cat_params)
+    if cat.save
+      render json: cat, status: status
+    else
+      render json: {errors: cat.errors.full_messages.join(' ')}, status: 422
+    end
+  end
+
+  def cat_params
+    params.permit(:name, :breed_id, :born_on, :image_url)
+  end
+
+end
