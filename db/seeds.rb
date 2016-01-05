84e4a4c9e251ff4738308b539517274df50ddd2e
Steven Cassidy <steven.cassidy@gmail.com>
2016-01-04 22:10:07 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,141 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Breed.create!(name: "Abyssinian", description:"Abyssinians aren't for those who want decorative cats to match the rust-colored carpet, or for those who want cats that enjoy ...")
+Breed.create!(name: "American Bobtail", description:"While the breed is still developing, breeders say that Bobtails are playful, energetic, and friendly, and possess an uncanny ...")
+Breed.create!(name: "American Curl", description:"Curls have qualities other than the whimsical ears to make them attractive pets. They are people cats that do not show any of ...")
+Breed.create!(name: "American Shorthair", description:"When describing the American Shorthair, the expression 'happy medium' springs to mind. These all-American cats are medium in ...")
+Breed.create!(name: "American Wirehair", description:"Wirehairs are people cats that crave human attention and affection. They are active without being hyper, and affectionate ...")
+Breed.create!(name: "Balinese", description:"Balinese cats are smart, sweet, and fun to be around. Like the Siamese, they are known for their ability to communicate vocally, ...")
+Breed.create!(name: "Bengal", description:"The Bengal may look like a wild cat, but breeders insist that the Bengal is as lovably friendly and docile as any full-blooded ...")
+Breed.create!(name: "Birman", description:"Birmans are affectionate, gentle, and faithful companions with an air of dignity that seems to invite adoration by their human ...")
+Breed.create!(name: "Bombay", description:"If an aloof, independent cat is what you're craving, this breed isn't for you. Bombays are attached to their family, and tend to ...")
+Breed.create!(name: "British Shorthair", description:"If you're looking for a cat that will loot your refrigerator and swing dizzily from your chandeliers, then the British Shorthair ...")
+Breed.create!(name: "Burmese", description:"Breeders and fanciers report that Burmese are amusing, playful, and super-smart, the perfect interactive cats for home, office, ...")
+Breed.create!(name: "Chartreux", description:"Known for their hunting prowess, Chartreux cats may have been taken in by those monks long ago to rid the monastery of vermin. ...")
+Breed.create!(name: "Cornish Rex", description:"Cornish Rexes are good for folks who like having their lives run by active, inquisitive, gazelle-like felines that love a good ...")
+Breed.create!(name: "Cymric", description:"The personality of the Cymric has won a strong following despite the breeding challenges. Cymrics are intelligent, fun-loving ...")
+Breed.create!(name: "Devon Rex", description:"Devons have been compared to pixies, elves, and, of course, space aliens for their jumbo-sized satellite-dish ears, large, ...")
+Breed.create!(name: "Egyptian Mau", description:"While fanciers might at first be attracted to the Egyptian Mau's beautiful spotted coat, most become enthusiasts because of the ...")
+Breed.create!(name: "Exotic Shorthair", description:"Some folks who don't appreciate that laid-back, mellow personality label Persians and their relatives 'furniture with fur', but ...")
+Breed.create!(name: "Havana Brown", description:"More distinctive than the muzzle, ears, or minklike coat is the Havana Brown?s personality. Although still quite rare and for ...")
+Breed.create!(name: "Himalayan", description:"Himmies, as fanciers call them, are perfect indoor cat companions. They are gentle, calm, and sweet-tempered, but they possess a ...")
+Breed.create!(name: "Japanese Bobtail", description:"Bobs make outstanding companions. They're curious, bold, intelligent, and alert, and easily adjust to new people, situations, ...")
+Breed.create!(name: "Javanese", description:"Javanese cats, like their Balinese relatives, are playful, devoted, and always eager to tell you their views on life, love, and ...")
+Breed.create!(name: "Korat", description:"Korats are not as vocal as their Siamese comrades; they have other ways of getting their wishes across. At dinnertime they'll ...")
+Breed.create!(name: "Maine Coon", description:"No breed has a monopoly on love and affection, but there's got to be some good reason that the Maine Coon has clawed his way up ...")
+Breed.create!(name: "Manx", description:"The Manx's personality is probably the reason the breed has won such a strong following despite the physical difficulties and ...")
+Breed.create!(name: "Munchkin", description:"For their part, Munchkins, oblivious to the controversy surrounding them, go on being just what they are, cats; self-assured and ...")
+Breed.create!(name: "Nebelung", description:"Nebelungs are mild, soft-spoken, and gentle. They are generally reserved around strangers, ranging from hiding-under-the-bed ...")
+Breed.create!(name: "Norwegian Forest Cat", description:"Natural athletes, Norwegian Forest Cats love to investigate counters, bookcases, and the loftiest peaks of their cat trees. ...")
+Breed.create!(name: "Ocicat", description:"Ocicats may look wild, but they are actually affectionate, curious, and playful, and possess a very strong devotion to their ...")
+Breed.create!(name: "Oriental", description:"The personality of the Oriental is as distinctive as the multicolored exterior. They are natural entertainers, full of ...")
+Breed.create!(name: "Persian", description:"If you want your cats bouncing around like hyperactive popcorn, don't buy a Persian. Persians are perfect companions, if you ...")
+Breed.create!(name: "Ragdoll", description:"Docile, mild-mannered, and congenial, Rag-dolls make ideal indoor companions. One of the nicest features of these cats is their ...")
+Breed.create!(name: "Russian Blue", description:"Russian Blues are gentle, genteel cats, and are usually reserved, or absent, when strangers are around. When they're with their ...")
+Breed.create!(name: "Scottish Fold", description:"Scottish Folds are intelligent, sweet-tempered, soft-spoken, and easily adaptable to new people and situations. They are very ...")
+Breed.create!(name: "Selkirk Rex", description:"Selkirks are fun-loving, mellow cats with a generous measure of love and affection for their human companions. Very people-...")
+Breed.create!(name: "Siamese", description:"Some cats seem to think that a purr or a friendly rub speaks louder than words. Siamese are not of this school of thought and ...")
+Breed.create!(name: "Siberian", description:"Siberians are affectionate cats with a good dose of personality and playfulness. They are amenable to handling, and breeders ...")
+Breed.create!(name: "Singapura", description:"Singapuras, happily unaware of the controversy surrounding them, go right on being what they are: pesky people pleasers. At home ...")
+Breed.create!(name: "Snowshoe", description:"Breeders brave enough to take on the Snowshoe challenge find that the cat pays back the effort in love and affection. Anyone ...")
+Breed.create!(name: "Somali", description:"With all the virtues of the Abyssinian and adorned by a gorgeous semi-long coat, the Somali is a beautiful and lively addition ...")
+Breed.create!(name: "Sphynx", description:"According to the French breed standard, the Sphynx is part monkey, part dog, part child, and part cat. The breed does seem to ...")
+Breed.create!(name: "Tonkinese", description:"The Tonkinese has a winsome personality, not surprising since the Burmese and Siamese are prized for their temperaments. ...")
+Breed.create!(name: "Turkish Angora", description:"Turkish Angora fanciers are as attached to their cats as their cats are to them. Angoras seem to invoke strong responses in ...")
+Breed.create!(name: "Turkish Van", description:"While you might be drawn to the Van for his fascination with water, you'll fall in love with the breed for his other qualities. '...")
+
+names = ["Alexa", "Alexis", "Alice", "Allie", "Amber", "Ambra", "Amelia",
+"Amethyst", "Andreas", "Andy", "Angel", "Angelica", "Angelina", "Anisa",
+"Annabelle", "Annie", "Ariel", "Armani", "Asante", "Asia", "Aspen", "Atessa",
+"Atilla", "Aurora", "Austin", "Ayla", "Baby", "Bandy", "Barney", "Bastian",
+"Belinda", "Bella", "Benny", "Bergen", "Bernie", "Bert", "Bianca", "Biddy",
+"Billy", "Blaze", "Bonnie", "Bonkie", "Bootsie", "Brox", "Bruce", "Bruno",
+"Bubbles", "Bucky", "Buddy", "Buffy", "Bumby", "Bunny", "Buster", "Buttons",
+"Callie", "Cameron", "Candy", "Carter", "Casey", "Casper", "Cassandra",
+"Castro", "Catalina", "Celeste", "Chaps", "Charlie", "Chase", "Chelsea",
+"Chester", "Chevy", "Chico", "Chili", "Chip", "Chloe", "Chrissie",
+"Christian", "Christopher", "Chuckie", "Chula", "Cinnamon", "Clairese",
+"Clara", "Clarabelle", "Clarence", "Clarice", "Claudette", "Clay",
+"Clementine", "Cleo", "Cleve", "Clyde", "Coco", "Colin", "Collette", "Colly",
+"Comet", "Cookie", "Cooper", "Cori", "Cornelius", "Cris", "Dalyla", "Danny",
+"Daphne", "Darren", "Dash", "Deanna", "Della", "Desdemona", "Dewey", "Dilly",
+"Dinero", "Dion", "Dixie", "Doby", "Dolby", "Dolly", "Dorothy", "Dottie",
+"Duchess", "Duckie", "Duke", "Dulcie", "Duncan", "Edgar", "Edward", "Elfie",
+"Elmo", "Elsie", "Elton", "Emily", "Ernie", "Eubie", "Eva", "Everest", "Evie",
+"Felix", "Fella", "Finn", "Fiona", "Flannel", "Florence", "Fluffy", "Fluke",
+"Forest", "Francesca", "Francie", "Francine", "Frankie", "Fred", "Freddie",
+"Frollo", "Garbo", "Garfield", "Gavin", "George", "Georgette", "Georgina",
+"Giggle", "Gilbert", "Giselle", "Gizmo", "Golda", "Goldie", "Gordo", "Gracie",
+"Greta", "Grettel", "Grimalkin", "Grizzabella", "Guinness", "Hamilton",
+"Hammy", "Happy", "Harley", "Harrison", "Harry", "Hecate", "Heidi", "Heloise",
+"Hemmingway", "Henrietta", "Henry", "Hershey", "Hilda", "Hobbes", "Hobbit",
+"Hocus", "Hollow", "Honey", "Honeyfoot", "Hooter", "Hope", "Howie", "Hozer",
+"Hubert", "Hubie", "Iggy", "Illapa", "Ilona", "Indigo", "Inky", "Iris",
+"Isabella", "Isis", "Ivan", "Jack", "Jackie", "Jaclyn", "Jade", "Jaime",
+"Jake", "James", "Janie", "January", "Jasmine", "Jasper", "Jay", "Jazzy",
+"Jean", "Jello", "Jenny", "Jerry", "Jesse", "Jessica", "Joe", "Jordan",
+"Joseph", "Josh", "Jules", "Junior", "Juve", "Kali", "Kareen", "Kasey", "Kat",
+"Kate", "Katie", "Katrina", "Keela", "Keilla", "Kelly", "Kiki", "Kilroy",
+"King", "Kingsley", "Kira", "Kirby", "Kisses", "Kit", "Kitten", "Kitty",
+"Kiwi", "Kobe", "Kookie", "Kruger", "Lady", "Lana", "Larry", "Laslo",
+"Laurie", "Leda", "Leo", "Lessie", "Lester", "Leticia", "Lexy", "Lily",
+"Little", "Loki", "Lola", "Lorenzo", "Lottie", "Lotus", "Louisa", "Lucky",
+"Lucy", "Lulu", "Luna", "Lyeta", "Madame", "Maddie", "Madeline", "Maggie",
+"Magilla", "Magnolia", "Maia", "Maize", "Mambo", "Martha", "Marvin",
+"Matisse", "Maxine", "Maybe", "Meezer", "Merlin", "Merlot", "Metaxa", "Mia",
+"Michelin", "Mickey", "Mike", "", "Miles", "Milky Way", "Millicent", "Milo",
+"Milt", "Minnie", "Miranda", "Mischa", "Mollie", "Molly", "Monkey", "Mookie",
+"Moose", "Morgan", "Moses", "Mouse", "Mouser", "Mouska", "Nala", "Nancy",
+"Neddy", "Neelix", "Nellie", "Nicholas", "Nicky", "Nicole", "Nigel", "Nikko",
+"Niles", "Nitty", "Noisy", "Norton", "Noxi", "Oliver", "Ollie", "Olyana",
+"Ophelia", "Opie", "Opus", "Oreo", "Oscar", "Osiris", "Othello", "Other",
+"Otto", "Owen", "Ozzie", "Pamela", "Panama", "Pandora", "Papillion",
+"Passion", "Pavarotti", "Peanut", "Pearl", "Penelope", "Penney", "Pepper",
+"Percy", "Perry", "Persia", "Phil", "Phoebe", "Photon", "Picasso", "Pickles",
+"Pigpen", "Pinocle", "Pip", "Pippi", "Pixel", "Pixie", "Poco", "Poodles",
+"Pookie", "Poppy", "Pounce", "Powder", "Prince", "Precious", "Princess",
+"Prinky", "Priscilla", "Prissy", "Puddy", "Pueblo", "Puff", "Puma", "Punkin",
+"Punky", "Purrcy", "Purrscilla", "Queenie", "Quiet", "Quincy", "Ralph",
+"Ramses", "Raven", "Raymond", "Reggie", "Remington", "Renoir", "Rickie",
+"Riker", "Riley", "Ripley", "Rishka", "Rocky", "Rodrigo", "Rogue", "Rolo",
+"Romeo", "Rory", "Rosemary", "Rosie", "Roxy", "Ruby", "Rudy", "Rune", "Rusty",
+"Ryder", "Sable", "Sabrina", "Sadie", "Saffron", "Sage", "Salem", "Salvador",
+"Samantha", "Samba", "Sammy", "Sandy", "Sapphire", "Sasha", "Sassy",
+"Scarlet", "Schatzie", "Schuster", "Scooter", "Scout", "Scully", "Seamus",
+"Sebastian", "Selfish", "Serena", "Serissa", "Shadow", "Shalimar", "Shammy",
+"Shannon", "Shaun", "", "Shelly", "Shiloh", "Shorter", "Sibby", "Sienna",
+"Sierra", "Silver", "Simarik", "Simba", "Simon", "Sirius", "Sirrus", "Sister",
+"Skeemer", "Skeeter", "Skipurr", "Skitz", "Slim", "Smiles", "Snickers", "Snow
+Flake", "Snow White", "Snowy", "Socrates", "Soey", "Soldier", "Sophie",
+"Spanky", "Spaz", "Spencer", "Spider", "Spielberg", "Spike", "Spooky",
+"Starkitty", "Stash", "Steamer", "Stella", "Steve", "Storm", "Strider",
+"Stripes", "Stripey", "Stubby", "Stuffy", "Stymie", "Sugar", "Sunita",
+"Sunshine", "Sushi", "Suzy", "Sweetie", "Sydney", "Sylvia", "Tabbu", "Taber",
+"Tabitha", "Taco", "Taffy", "Tang", "Tangerine", "Tar Baby", "Tasha", "Tevye",
+"Texas", "Thelma", "Theo", "Theodore", "Thumper", "Tia", "Tibbs", "Tiga",
+"Tiger", "Tigg", "Tigger", "Tilly", "Timber", "Timmy", "Tipper", "Tippy",
+"Tisha", "Titter", "Toad", "Toby", "Tom", "Tomba", "Toonces", "Tooshy",
+"Tootie", "Tootsie", "Torrance", "Tribble", "Trouble", "Trudy", "Tucker",
+"Tuffy", "Tum", "Tuna", "Turbo", "Twinkie", "Twinkle", "Twister", "Ursula",
+"Valentina", "Vegas", "Venus", "Veto", "Vicky", "Victoria", "Vince",
+"Vincent", "Vinny", "Violet", "Vivien", "Vixen", "Waldo", "Wallace", "Wally",
+"Wampuss", "Warehouse", "Weezer", "Wendy", "Wesley", "Whimsy", "Whiskers",
+"Whisper", "Whitney", "Willie", "Willimena", "Willow", "Wilson", "Winky",
+"Wishes", "Wolfie", "Xavier", "Xena", "Yashin", "Yvette", "Zelda", "Zena",
+"Zenobia", "Zeta", "Zeus", "Ziggy", "Zigmund", "Ziggy", "Zinzan", "Ziti",
+"Zoe", "Zoom", "Zorro"]
+
+breeds = Breed.all
+
+def time_rand from = 1104555600.0, to = Time.now
+  Time.at(from + rand * (to.to_f - from.to_f))
+end
+
+
+20.times do
+  c = Cat.create!(name:names.sample, breed: breeds.sample, born_on:time_rand)
+  c.reload
+  c.image_url = "http://do.scass.com/images/kittens/#{c.id % 20}.jpg"
+  c.save!
+end
